f87514a0f8a046473b7286bdc857a8d92b95b4d3
OG24 <ogurel24@gmail.com>
2022-10-05 15:49:35 +0200

    Feature

---
+++ src/test/java/features/HeaderFunctionality.feature
@@ -0,0 +1,4 @@
+Feature: Title Functionality
+  Scenario: Title Functionality which redirects to Homepage whenever clicked
+    Given Click the elements section
+    Then Click the the header section and check whether it is the homepage
--- src/test/java/steps/ElementsPageSteps.java
+++ src/test/java/steps/ElementsPageSteps.java
@@ -1,20 +1,12 @@
 package steps;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import pages.ElementsPage;
 
 public class ElementsPageSteps {
     ElementsPage elementsPage = new ElementsPage();
 
-    @Before
-    public void start(){driverManager.init();}
-
-    @After
-    public void kill(Scenario scenario){driverManager.kill(scenario);}
-
-    @Given("Click the the header section and check whether it is the homepage")
+    @Then("Click the the header section and check whether it is the homepage")
     public void clickElementsSection() {
         elementsPage.click_header();
     }
